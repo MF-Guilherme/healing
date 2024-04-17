5bf6769383e8a54b59e00ab70b209d0a1a89c91f
MF-Guilherme <guimontenegro23@yahoo.com.br>
2024-04-17 11:52:19 -0300

    feat(usuarios/login): Tela e funcionalidade de login implementadas

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
-from django.contrib import messages
+from django.contrib import messages, auth
 
 def cadastro(request):
     if request.method == 'GET':
@@ -34,4 +34,21 @@ def cadastro(request):
             password=senha
         )
 
+        return redirect('/usuarios/login')
+    
+
+def login_view(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/pacientes/home')
+
+        messages.add_message(request, constants.ERROR, 'Usuário ou senha inválidos')
         return redirect('/usuarios/login')
