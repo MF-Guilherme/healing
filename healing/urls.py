c1c2ba7e4fed1183f33d4a97307a73f94520885e
MF-Guilherme <guimontenegro23@yahoo.com.br>
2024-05-02 16:33:31 -0300

    feat(home): redirecionando a url raiz para home

--- healing/urls.py
+++ healing/urls.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -9,4 +10,5 @@ urlpatterns = [
     path('usuarios/', include('usuarios.urls')),
     path('medicos/', include('medico.urls')),
     path('pacientes/', include('paciente.urls')),
+    path('', lambda request: redirect('/pacientes/home'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
